2dada962da16c82fdb46d4b1cd766253c5019176
mrahman7520 <mohammad.rahman6540@gmail.com>
2023-06-18 22:43:31 -0500

    runner update

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -6,10 +6,11 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = {"src\\test\\resources\\features"},	
-		glue = {"step_definitions", "Hooks"},
-		tags = "@smoke",
-		plugin =  {"pretty", "html:target/cucumber-reports"}
+		features = {"src/test/resources/features"}, 
+		glue = {"step_definitions", "Hooks"}, 
+	        tags = "@smoke",
+              //  {"pretty", "html:test-output", "json:target/cucumber-report/cucumber.json" }
+	      plugin =  {"pretty", "html:target/cucumber-reports"}
 )
 public class Runner {
 
